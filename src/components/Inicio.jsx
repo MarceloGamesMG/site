057ec7450c156d592322e5f7dde0fe801c75c2d3
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-01-31 18:04:28 -0300

    site

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -9,21 +9,19 @@ export default function Inicio() {
         <div className='bg-bg2 bg-cover bg-no-repeat brightness-110 w-screen border-second-tema-site drop-shadow-verde border-b-2 flex justify-center items-center  md:pt-5'>
             <div className="md:w-[1000px]">
                 <Nav />
-                <div className='flex flex-col md:flex-row md:pt-20 items-center justify-center pt-5 text-center '>
+                <div className=' flex flex-col md:flex-row md:pt-20 items-center justify-center pt-5 text-center '>
                     <div className="space-y-10 md:w-[534px] md:text-start">
-                        <h1 className="text-2xl md:text-6xl font-bold font-minhaFonte">
+                        <h1 className="text-2xl text-center md:text-6xl font-bold font-minhaFonte">
                             Lorem ipsum dolor sit.
                         </h1>
-                        <h2>
+                        <h2 className='text-center'>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, facilis consequatur nesciunt expedita, quos distinctio
                         </h2>
                         <div className='flex items-center justify-center pb-20'>
                         <Botao informacao={"Ler mais"} />
                         </div>
                     </div>
-                    <div className='flex justify-end '>
-                        <Image src={garoto} alt='garoto' className="w-96 -mt-40 md:mt-4 md:w-[800px] drop-shadow-verdeB" />
-                    </div>
+                    
                 </div>
                 
             </div>
