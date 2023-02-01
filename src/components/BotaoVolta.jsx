964fb756ba20a72c36e5142d46b309237dfcfc9a
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-02-01 14:45:25 -0300

    site

--- src/components/BotaoVolta.jsx
+++ src/components/BotaoVolta.jsx
@@ -1,15 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleUp } from "@fortawesome/free-solid-svg-icons";
 export default function BotaoVolta() {
-
-   
     return (
         <div>
-            <a href="#inicio" className='bg-branco fixed bottom-28 right-5 z-50  hover:drop-shadow-verdeB outline-double outline-second-tema-site rounded-full text-second-tema-site'>
+            <a href="#inicio" className='bg-branco fixed bottom-5 left-5 z-50  hover:drop-shadow-verdeB outline-double outline-second-tema-site rounded-full text-second-tema-site'>
                 <FontAwesomeIcon icon={faArrowAltCircleUp} className='h-12'/>
             </a>
         </div>
-
     )
-
 }
