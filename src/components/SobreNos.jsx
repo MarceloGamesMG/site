b95f63fbfb8b42af6d26c412b000fd6e70ae8928
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-02-03 15:47:36 -0300

    site

--- src/components/SobreNos.jsx
+++ src/components/SobreNos.jsx
@@ -3,7 +3,7 @@ export default function SobreNos(){
         <div id='sobre' className="flex flex-col justify-center items-center w-[320px] md:w-[700px] pt-16 pb-28">
             <h1 className='text-4xl md:text-6xl font-minhaFonte mb-20'>Sobre nós</h1>
             <p className='text-center'>
-            A Marcelo Games atua no mercado desde 2013, possuímos grande experiência em revenda de produtos de informática, eletrônicos e acessórios. Procurando produtos com bons preços e qualidade? Você está no lugar certo! Nossa missão é oferecer à todos os clientes produtos das melhores marcas com o menor preço do mercado, tudo isso pensando na satisfação de nossos clientes.<br/> Consulte nosso catálogo, acompanhe nossas Redes Sociais, e entre em contato conosco. Será um prazer atendê-lo! <br/> <br/><span className='text-second-tema-site drop-shadow-verdeB font-bold'>Que tal aproveitar as ofertas disponíveis em nossa loja e garantir aquele produto que tanto precisa?</span> 
+            A Marcelo Games atua no mercado desde 2013, possuímos grande experiência em revenda de produtos de informática, eletrônicos e acessórios. Procurando produtos com bons preços e qualidade? Você está no lugar certo! Nossa missão é oferecer à todos os clientes produtos das melhores marcas com o menor preço do mercado, tudo isso pensando na satisfação de nossos clientes.<br/> Consulte nosso catálogo, acompanhe nossas Redes Sociais, e entre em contato conosco. <br/> Será um prazer atendê-lo! <br/> <br/><span className='text-second-tema-site drop-shadow-verdeB font-bold'>Que tal aproveitar as ofertas disponíveis em nossa loja e garantir aquele produto que tanto precisa?</span> 
             </p>
         </div>
     )
